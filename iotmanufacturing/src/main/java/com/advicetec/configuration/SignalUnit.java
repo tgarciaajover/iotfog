49f2bb5b0fdbe9ed440a3454cb4b352f386c5f72
amarentes <la.marentes455@uniandes.edu.co>
2017-04-25 07:21:29 -0500

    SignalUnit REST api

--- iotmanufacturing/src/main/java/com/advicetec/configuration/SignalUnit.java
+++ iotmanufacturing/src/main/java/com/advicetec/configuration/SignalUnit.java
@@ -1,16 +1,34 @@
 package com.advicetec.configuration;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.JsonSerializer;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 public class SignalUnit extends ConfigurationObject
 {
-	private String descr;
+
+	
+	@JsonProperty("descr") private String descr;
+	
+	@JsonProperty("create_date") 
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
 	private LocalDateTime create_date;
 	
-	public SignalUnit(Integer id) {
+	@JsonCreator
+	public SignalUnit(@JsonProperty("id") Integer id) {
 		super(id);
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
@@ -34,5 +52,55 @@ public class SignalUnit extends ConfigurationObject
 	public void setCreate_date(LocalDateTime create_date) {
 		this.create_date = create_date;
 	}
+	
+	public String toJson()
+	{
+		ObjectMapper mapper = new ObjectMapper();
+			
+		String jsonInString=null;
+		try {
+			
+			jsonInString = mapper.writeValueAsString(this);
+			
+			
+		} catch (JsonGenerationException e) {
+			// TODO: log the error
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return jsonInString;
+	}
+	
+	
+	public void fromJSON(String json){
+		
+		ObjectMapper mapper = new ObjectMapper();
+		
+		//Convert object to JSON string and pretty print
+		SignalUnit unitTemp;
+		try {
+		
+			unitTemp = mapper.readValue(json, SignalUnit.class);
+			setId(unitTemp.getId());
+			setDescr(unitTemp.getDescr());
+			setCreate_date(unitTemp.getCreate_date());
+		
+		
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}	
 	
 }
