52a637c56f380e5fc1193340071808167900b295
lamarentes <lamarentes@advmain>
2017-07-14 16:37:00 -0500

    OEE implementation

--- iotmanufacturing/src/test/java/com/advicetec/iot/rest/IntervalTest.java
+++ iotmanufacturing/src/test/java/com/advicetec/iot/rest/IntervalTest.java
@@ -79,8 +79,9 @@ public class IntervalTest {
 		
 		MeasuringState state = MeasuringState.OPERATING;
 		ReasonCode reason = new ReasonCode(1, "Reason 001");
+		Double rate = new Double(0.0);
 		
-		StateInterval interval = new StateInterval(state, reason, int1, 2,MeasuredEntityType.MACHINE);
+		StateInterval interval = new StateInterval(state, reason, int1, 2,MeasuredEntityType.MACHINE, rate);
 		
 		String json = interval.toJson();
 		System.out.println(json);
