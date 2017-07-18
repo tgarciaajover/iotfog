5c8bf9958a0ec72e6f958aff09a5115c352c7615
lamarentes <lamarentes@advmain>
2017-07-18 11:22:59 -0500

    Change for OEE implementation

---
+++ iotmanufacturing/src/main/java/com/advicetec/measuredentitity/OEEAggregationManager.java
@@ -0,0 +1,43 @@
+package com.advicetec.measuredentitity;
+
+import java.sql.SQLException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.advicetec.core.Configurable;
+
+public class OEEAggregationManager  extends Configurable {
+
+	
+	static Logger logger = LogManager.getLogger(OEEAggregationManager.class.getName());
+	
+	private static OEEAggregationManager instance = null;
+	private OEEAggregationContainer oeeAggregations;
+	
+	public OEEAggregationManager() {
+		super("OEEAggregation");
+
+		String driver = properties.getProperty("driver");
+		String server = properties.getProperty("server");
+		String user = properties.getProperty("user");
+		String password = properties.getProperty("password");
+
+		oeeAggregations = new OEEAggregationContainer(driver, server, user, password);
+			
+	}
+
+	public static OEEAggregationManager getInstance()
+	{
+		if(instance == null){
+			instance = new OEEAggregationManager();
+		}
+		return instance;
+	}	
+	
+	public OEEAggregationContainer getOeeAggregationContainer()
+	{
+		return this.oeeAggregations;
+	}
+	
+}
